2014fd16d00d4e7a8ad2ff77f1ab875bd0cc5059
Rhys Evans <wheresrhys@gmail.com>
2016-03-29 23:36:17 +0100

    trick to get parallel loaded core and page specific css

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,83 +1,42 @@
 /* globals self, caches, clients */
 'use strict';
-const cacheName = 'server-push-polyfill';
+const cacheName = 'css-trick';
 
-const inFlight = {}
 
-function putInCache(request, res) {
-	const copy = res.clone();
-	return caches.open(cacheName)
-		.then(function (cache) {
-			return cache.put(request, copy);
-		});
-}
 
-self.addEventListener('install',function(event) {
-	console.log('install')
+self.addEventListener('install', function () {
+	// console.log('install')
 });
 
 self.addEventListener('activate', function() {
-	console.log('activate')
+	// console.log('activate')
 	if (self.clients && clients.claim) {
 		clients.claim();
 	}
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
 	var request = event.request;
-	console.log('real fetch', request.url)
-	if (/\.(css|js)/.test(request.url)) {
-		console.log('trying to find in cache', request.url)
+	// console.log('real fetch', request.url)
+	if (/\?parts=/.test(request.url)) {
+		const urls = /\?parts=\[([^\]]+)\]/.exec(request.url)[1].split(',');
+
+		// console.log('trying to find in cache', request.url)
 		return event.respondWith(
-      caches.match(request)
-        .then(function (response) {
-
-        	if (response) {
-        		console.log('delivering cached', request.url);
-        		return response.clone();
-        	}
-        	if (inFlight[request.url]) {
-						console.log('delivering server push', request.url);
-						return inFlight[request.url];
+			Promise.all(
+				urls.map(url => fetch(url).then(res => res.text()))
+			)
+				.then(texts => texts.join('\n'))
+				.then(text => new Response(text, {
+					status: 200,
+					headers: {
+						'Content-Type':'text/css; charset=UTF-8'
 					}
-					console.log('failed to find in cache', request.url)
-					return fetch(request);
-
-        })
-    );
+				})
+			)
+		);
 	}
 
-	event.respondWith(
-		fetch(request)
-			.then(res => {
-				const pushes = res.headers.get('x-server-push');
-
-				if (pushes) {
-					pushes.split(',').forEach(path => {
-						path = 'http://localhost:3000' + path
-						const pushyRequest = new Request(path);
-						caches.match(pushyRequest)
-							.then(res => {
-								console.log('back from cache', path)
-								if (!res) {
-									console.log('sending server push', path)
-									inFlight[path] = fetch(pushyRequest)
-										.then(res => {
-											console.log('server push arrived', path);
-											putInCache(pushyRequest, res)
-												.then(() => {
-													console.log('deleting inFlight', path)
-													delete inFlight[path];
-												})
-											return res.clone()
-										})
-								}
-							})
-
-					})
-				}
-				return res;
-			})
-	);
+	event.respondWith(fetch(request))
 	return;
 });
--- server.js
+++ server.js
@@ -7,65 +7,33 @@ app.use(function (req, res, next) {
 	res.set('Cache-control', 'max-age=0,no-cache')
 	next();
 });
-app.get
-app.get('/polyfill-demo', function (req, res) {
-	const minute = Math.floor(Date.now()/20000);
-	let html = '';
-
-	const article = `
-		<article class="markdown-body entry-content" itemprop="mainContentOfPage"><h1><a id="user-content-proof-of-concept-for-using-service-worker-to-polyfill-server-push" class="anchor" href="#proof-of-concept-for-using-service-worker-to-polyfill-server-push" aria-hidden="true"><span class="octicon octicon-link"></span></a>Proof of concept for using service worker to polyfill server push</h1>
-
-<h2><a id="user-content-how-it-works" class="anchor" href="#how-it-works" aria-hidden="true"><span class="octicon octicon-link"></span></a>How it works</h2>
-
-<p>The server sets an <code>x-server-push</code> header on the main html response which lists any files which should be 'pushed' by the server. On fetch the service worker checks for this header and fires off a response for each path specified, storing the requests in memory.</p>
-
-<p>When the browser then attempts to fetch these resources they are intercepted by the service worker and respond with the request that was kicked off earlier by the service worker.</p>
-
-<h2><a id="user-content-impact" class="anchor" href="#impact" aria-hidden="true"><span class="octicon octicon-link"></span></a>Impact</h2>
-
-<h3><a id="user-content-with" class="anchor" href="#with" aria-hidden="true"><span class="octicon octicon-link"></span></a>With</h3>
-
-<p>Note the very fast response for the js. The effect on css is negligeable, though this test page has an unrealistically tiny <code>&lt;head&gt;</code>, so in the real world there could be a small but very real improvement
-
-<h3><a id="user-content-without" class="anchor" href="#without" aria-hidden="true"><span class="octicon octicon-link"></span></a>Without</h3>
-
-
-<h2><a id="user-content-limitations" class="anchor" href="#limitations" aria-hidden="true"><span class="octicon octicon-link"></span></a>Limitations</h2>
-
-<ul>
-<li>Still have to wait for the round trip for html to send its headers before the 'push'es can begin.
-The most time you'll save is the time it takes to download (inbound journey online) and parse your html page,
-but even on high performance websites this will be a saving of ~100ms... and if you're on a high
-performance website you'll already know ~100ms is a respectable dent</li>
-<li>Possibly a bad idea to do this for too many requests unless your site is already on http2 -
-kicking off an extra early http request for js could delay the loading of other resources, eg. images, fonts</li>
-</ul>
-
-<h2><a id="user-content-todo" class="anchor" href="#todo" aria-hidden="true"><span class="octicon octicon-link"></span></a>TODO</h2>
-
-<ul>
-<li>interacting properly with cache - the 'push' should store in cache and the real requests from the page should check cache before checking the inFlight object</li>
-</ul>
-</article>
-`;
-let i = 0;
-while (i++ < 20) {
-	html+= article;
-}
-
-	res.set('x-server-push', `/main.css?hash=${minute},/main.js?hash=${minute}`)
+app.get('/page1', function (req, res) {
 	res.send(`<!DOCTYPE html>
 		<html>
 			<head>
-				<link href="/main.css?hash=${minute}" rel="stylesheet" />
+				<link href="/css-trick/main.css?parts=[/css-trick/shared.css,/css-trick/page1.css]" rel="stylesheet" />
 			</head>
 			<body>
-		${html}
-<script src="/main.js?hash=${minute}"></script>
+		<a href="/page2">page2</a>
+<script src="/main.js"></script>
 			</body>
 		</html>
 	`)
-})
+});
+
+app.get('/page2', function (req, res) {
+	res.send(`<!DOCTYPE html>
+		<html>
+			<head>
+				<link href="/css-trick/main.css?parts=[/css-trick/shared.css,/css-trick/page2.css]" rel="stylesheet" />
+			</head>
+			<body>
+		<a href="/page1">page1</a>
+<script src="/main.js"></script>
+			</body>
+		</html>
+	`)
+});
 
 app.listen(process.env.PORT || 3000, function () {
 	console.log('listening on 3000');
